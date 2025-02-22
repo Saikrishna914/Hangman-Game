a0698d97d1c5ee714c0f053561b246f2b5e33eb8
Sai Krishna <saikrishnajaihind2424@gmail.com>
2025-02-22 19:59:17 +0530

    Added react router dom

--- src/components/TextInputForm/TextInputFormContainer.jsx
+++ src/components/TextInputForm/TextInputFormContainer.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import TextInputForm from "./TextInputForm";
+import { useNavigate } from "react-router-dom";
 
 function TextInputFormContainer(){
 
     const [inputType,setInputType]=useState("password");
+    const [value,setValue]=useState("");
+    
+    const navigate = useNavigate();
 
     function handleFormSubmit(event){
         event.preventDefault();
-        console.log("form submitted");
+        console.log("form submitted", value);
+        if(value){
+            navigate("/play");
+        }
     }
     
     function handleTextInputChange(event){
         console.log("Text Input Changed");
         console.log(event.target.value);
+        setValue(event.target.value);
     }
 
     function handleShowHideClick(event){
--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,8 +1,11 @@
+import TextInputFormContainer from "../components/TextInputForm/TextInputFormContainer"
+
 function StartGame(){
     return (
         <>
             <h1>Start Game</h1>
             {/*Your comment goes here */}
+            <TextInputFormContainer/>
         </>
     )
 }
